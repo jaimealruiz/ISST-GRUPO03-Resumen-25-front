f8169ebad5dddcc2315a1d8a64747ce9883ad057
looreea <alicialorea.macias@alumnos.upm.es>
2025-04-02 01:32:44 +0200

    cambios en catalogo, dashboard y booksummary

--- frontend/src/pages/Catalogo.js
+++ frontend/src/pages/Catalogo.js
@@ -1,34 +1,47 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from './AuthContext';
-import API from './api';
+import { useAuth} from '../context/AuthContext';
+import API from '../helpers/axios_helper';
 
 const Catalogo = () => {
-  const { auth } = useContext(AuthContext);
-  const [resumenes, setResumenes] = useState([]);
+  const { user } = useAuth();
+    const [resumenes, setResumenes] = useState([]);
 
   useEffect(() => {
-    API.get('/resumenes')
-      .then(response => {
-        // Suponiendo que response.data sea el array de resumenes
-        if (!auth.isAuthenticated) {
-          setResumenes(response.data.slice(0, 3));
-        } else {
-          setResumenes(response.data);
-        }
-      })
-      .catch(error => console.error(error));
-  }, [auth.isAuthenticated]);
+    API.get('/userdata/lista-resumen.json')
+    .then((response) => {
+      // Se asume que la respuesta tiene la propiedad "resumenes"
+      setResumenes(response.data.resumenes);
+    })
+    .catch((error) => console.error('Error al cargar los resúmenes:', error));
+}, []);
 
-  return (
-    <div>
-      {resumenes.map(item => (
-        <div key={item.id}>
-          <img src={item.portadaUrl} alt={item.titulo} />
-          <h3>{item.titulo}</h3>
+const resGratuitos = ["Who Moved My Cheese?", "Nunca te pares", "Hábitos Atómicos"];
+
+const displayedResumenes = user
+? resumenes
+: resumenes.filter((resumen) => resGratuitos.includes(resumen.titulo));
+
+return (
+  <div className="catalog-container">
+    <h1>Catálogo</h1>
+    <div className="books-grid">
+      {displayedResumenes.map((resumen) => (
+        <div key={resumen.id} className="book-item">
+          <h3>{resumen.titulo}</h3>
+          <p>{resumen.autor.email}</p>
+          <a 
+            href={resumen.archivoUrl} 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="ver-resumen"
+          >
+            Ver resumen
+          </a>
         </div>
       ))}
     </div>
-  );
+  </div>
+);
 };
 
 export default Catalogo;
