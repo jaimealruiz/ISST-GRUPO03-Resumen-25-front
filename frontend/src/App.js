c3549f6af57929bb0995b5de301046470ddc7fb8
looreea <alicialorea.macias@alumnos.upm.es>
2025-04-01 17:08:34 +0200

    feat(login): cambios en login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css'; // Deja el archivo de estilos si lo necesitas
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         {/* Aquí puedes agregar un header global si lo necesitas, pero lo básico ya está en Home */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import React, { useNavigate, useState } from 'react';
 //import { useNavigate } from "react-router-dom";
 
